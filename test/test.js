7f920ff6080b6c3f07ebfea6c5d36d119509ccba
Michael Seno <michaelseno@m800.com>
2018-05-28 15:18:21 +0800

    modified url

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var supertest = require("supertest")
   , should = require("should")
-  , server = supertest.agent("http://localhost:3000")
+  , server = supertest.agent("http://google.com/")
 
 describe("Server launch test",function(){
   it("should return landing page",function(done){
