0055788e26a68f4714d3e76fa6026b641e109392
Michael Seno <michaelseno@m800.com>
2018-05-29 13:46:20 +0800

    revert changes

--- test/test.js
+++ test/test.js
@@ -3,13 +3,43 @@ var supertest = require("supertest")
   , server = supertest.agent("http://localhost:8080")
 
 describe("Server launch test",function(){
-  it("should return landing page",function(done){
+  it("should return landing page 1",function(done){
     server
     .get("/")
     .expect("Content-type",/text/)
-    .expect(200) 
+    .expect(403) 
     .end(function(err,res){
-      res.status.should.equal(200)
+      res.status.should.equal(403)
+      done()
+    })
+  })
+  it("should return landing page 2",function(done){
+    server
+    .get("/")
+    .expect("Content-type",/text/)
+    .expect(403) 
+    .end(function(err,res){
+      res.status.should.equal(403)
+      done()
+    })
+  })
+  it("should return landing page 3",function(done){
+    server
+    .get("/")
+    .expect("Content-type",/text/)
+    .expect(403) 
+    .end(function(err,res){
+      res.status.should.equal(403)
+      done()
+    })
+  })
+  it("should return landing page 4",function(done){
+    server
+    .get("/")
+    .expect("Content-type",/text/)
+    .expect(403) 
+    .end(function(err,res){
+      res.status.should.equal(403)
       done()
     })
   })
