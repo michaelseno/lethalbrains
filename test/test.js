ad05c95370ad771bea54a04d0ebdc740bc129588
Michael Seno <michaelseno@m800.com>
2018-05-29 17:32:21 +0800

    update test case

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var supertest = require("supertest")
   , server = supertest.agent("http://localhost:8080")
 
 describe("Server launch test",function(){
-  it("should return landing page 1",function(done){
+  it("should return landing page",function(done){
     server
     .get("/")
     .expect("Content-type",/text/)
@@ -12,37 +12,7 @@ describe("Server launch test",function(){
       res.status.should.equal(403)
       done()
     })
-  })
-  it("should return landing page 2",function(done){
-    server
-    .get("/")
-    .expect("Content-type",/text/)
-    .expect(403) 
-    .end(function(err,res){
-      res.status.should.equal(403)
-      done()
-    })
-  })
-  it("should return landing page 3",function(done){
-    server
-    .get("/")
-    .expect("Content-type",/text/)
-    .expect(403) 
-    .end(function(err,res){
-      res.status.should.equal(403)
-      done()
-    })
-  })
-  it("should return landing page 4",function(done){
-    server
-    .get("/")
-    .expect("Content-type",/text/)
-    .expect(403) 
-    .end(function(err,res){
-      res.status.should.equal(403)
-      done()
-    })
-  })
+  }) 
 })
 
 
