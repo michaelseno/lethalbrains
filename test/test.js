aa52811edf8f2248558b8b6fe4f2122d5981d425
Michael Seno <michaelseno@m800.com>
2018-05-30 08:56:26 +0800

    modified script

--- test/test.js
+++ test/test.js
@@ -3,7 +3,47 @@ var supertest = require("supertest")
   , server = supertest.agent("http://localhost:8080")
 
 describe("Server launch test",function(){
-  it("should return landing page",function(done){
+  it("should return landing page 1st attempt",function(done){
+    server
+    .get("/")
+    .expect("Content-type",/text/)
+    .expect(403) 
+    .end(function(err,res){
+      res.status.should.equal(403)
+      done()
+    })
+  }) 
+  it("should return landing page 2nd attempt",function(done){
+    server
+    .get("/")
+    .expect("Content-type",/text/)
+    .expect(403) 
+    .end(function(err,res){
+      res.status.should.equal(403)
+      done()
+    })
+  }) 
+  it("should return landing page 3rd attempt",function(done){
+    server
+    .get("/")
+    .expect("Content-type",/text/)
+    .expect(403) 
+    .end(function(err,res){
+      res.status.should.equal(403)
+      done()
+    })
+  }) 
+  it("should return landing page 4th attempt",function(done){
+    server
+    .get("/")
+    .expect("Content-type",/text/)
+    .expect(403) 
+    .end(function(err,res){
+      res.status.should.equal(403)
+      done()
+    })
+  }) 
+  it("should return landing page 5th attempt",function(done){
     server
     .get("/")
     .expect("Content-type",/text/)
