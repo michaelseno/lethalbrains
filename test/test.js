30552fd2788bf7ff9c165dbadfb1f5bebc30ae24
Michael Seno <michaelseno@m800.com>
2018-06-08 14:21:54 +0800

    modified script

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var supertest = require("supertest")
   , should = require("should")
   , server = supertest.agent("http://localhost:8080")
 
-describe("Mocha Automation first suite",function(){
+describe("Mocha Automation",function(){
   it("should return landing page 1st attempt",function(done){
     server
     .get("/")
@@ -54,60 +54,3 @@ describe("Mocha Automation first suite",function(){
     })
   }) 
 })
-
-describe("Mocha Automation second suite",function(){
-  it("should return landing page 1st attempt",function(done){
-    server
-    .get("/")
-    .expect("Content-type",/text/)
-    .expect(403) 
-    .end(function(err,res){
-      res.status.should.equal(403)
-      done()
-    })
-  }) 
-  it("should return landing page 2nd attempt",function(done){
-    server
-    .get("/")
-    .expect("Content-type",/text/)
-    .expect(403) 
-    .end(function(err,res){
-      res.status.should.equal(403)
-      done()
-    })
-  }) 
-  it("should return landing page 3rd attempt",function(done){
-    server
-    .get("/")
-    .expect("Content-type",/text/)
-    .expect(403) 
-    .end(function(err,res){
-      res.status.should.equal(403)
-      done()
-    })
-  }) 
-  it("should return landing page 4th attempt",function(done){
-    server
-    .get("/")
-    .expect("Content-type",/text/)
-    .expect(403) 
-    .end(function(err,res){
-      res.status.should.equal(403)
-      done()
-    })
-  }) 
-  it("should return landing page 5th attempt",function(done){
-    server
-    .get("/")
-    .expect("Content-type",/text/)
-    .expect(403) 
-    .end(function(err,res){
-      res.status.should.equal(403)
-      done()
-    })
-  }) 
-})
-
-
-
-
